665560993e7a4a5b21e0853788ee6d61af28dd91
Sainikhil450 <154584709+Sainikhil450@users.noreply.github.com>
2023-12-22 14:48:24 +0530

    Add files via upload

---
+++ Variables/Variable.py
@@ -0,0 +1,25 @@
+# integer variable
+age=22
+print("it is integer variable :-",age)
+print("**********************************")
+
+# Float variable
+height=5.9
+print("it is Floor variable :-",height)
+print("**********************************")
+
+# String Variable
+name="Nikhil"
+print("it is String variable :-",name)
+print("***********************************")
+
+# Boolean Variable
+is_student=True
+print("am i student ?",is_student)
+print("***********************************")
+
+# Multiple variable assign
+x,y,z=10,20,30
+print("it is Multiple variable assign in a single line:-",x,y,z)
+
+
